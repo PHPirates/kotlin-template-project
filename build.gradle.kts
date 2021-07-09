3044a26d0b33644c202e1562a72e4314f32785e5
Thomas Schouten <hollandpirates@gmail.com>
2021-07-09 16:20:01 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,26 @@ version = "0.0"
 
 plugins {
 
-    val kotlinVersion = "1.4.0"
+    val kotlinVersion = "1.5.20"
 
     application
     kotlin("jvm") version kotlinVersion
     java // Required by at least JUnit.
 
     // Plugin which checks for dependency updates with help/dependencyUpdates task.
-    id("com.github.ben-manes.versions") version "0.36.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
-    id("se.patrikerdes.use-latest-versions") version "0.2.15"
+    id("se.patrikerdes.use-latest-versions") version "0.2.17"
 
     // Test coverage
     jacoco
 
     // Upload jacoco coverage reports to coveralls
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.10"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.13"
 
     // https://openjfx.io/openjfx-docs/#gradle
-    id("org.openjfx.javafxplugin") version "0.0.9"
+    id("org.openjfx.javafxplugin") version "0.0.10"
 }
 
 javafx {
@@ -39,9 +39,9 @@ dependencies {
     implementation(kotlin("test-junit"))
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-console:1.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0-M1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0-M1")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.8.0-M1")
 
     // Kotlintest
     testImplementation("io.kotlintest:kotlintest-core:3.4.2")
@@ -49,8 +49,8 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 
     // JavaFX tests using TestFX
-    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
-    testImplementation("org.testfx:testfx-junit:4.0.16-alpha")
+    testImplementation("org.testfx:testfx-core:4.0.15-alpha")
+    testImplementation("org.testfx:testfx-junit:4.0.15-alpha")
     // Only needed for headless testing.
 //    testImplementation("org.testfx:openjfx-monocle:8u76-b04") // jdk-9+181 for Java 9
 
@@ -62,7 +62,6 @@ dependencies {
 repositories {
     mavenCentral()
     mavenLocal()
-    jcenter()
 }
 
 // Test coverage reporting for coveralls.
@@ -71,10 +70,10 @@ tasks {
     "jacocoTestReport"(JacocoReport::class) {
         reports {
             // To be read by humans
-            html.isEnabled = true
+            html.required.set(true)
             // To be read by Coveralls etc.
-            xml.isEnabled = true
-            xml.destination = file("$buildDir/reports/jacoco/test/jacocoTestReport.xml")
+            xml.required.set(true)
+            xml.outputLocation.set(file("$buildDir/reports/jacoco/test/jacocoTestReport.xml"))
         }
 
     }
