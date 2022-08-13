763135229fc3b95d2c095db979e0c8893ce194dc
Thomas Schouten <hollandpirates@gmail.com>
2022-08-13 17:29:03 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,22 +10,22 @@ plugins {
     java // Required by at least JUnit.
 
     // Plugin which checks for dependency updates with help/dependencyUpdates task.
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
-    id("se.patrikerdes.use-latest-versions") version "0.2.17"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
 
     // Test coverage
     jacoco
 
     // Upload jacoco coverage reports to coveralls
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.13"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.14"
 
     // New test coverage plugin
-    id("org.jetbrains.kotlinx.kover") version "0.4.2"
+    id("org.jetbrains.kotlinx.kover") version "0.6.0-Beta"
 
     // https://openjfx.io/openjfx-docs/#gradle
-    id("org.openjfx.javafxplugin") version "0.0.10"
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
 javafx {
@@ -42,9 +42,9 @@ dependencies {
     implementation(kotlin("test-junit"))
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0-M1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0-M1")
-    testRuntimeOnly("org.junit.platform:junit-platform-console:1.8.0-M1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.9.0")
 
     // Kotlintest
     testImplementation("io.kotlintest:kotlintest-core:3.4.2")
@@ -52,14 +52,14 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 
     // JavaFX tests using TestFX
-    testImplementation("org.testfx:testfx-core:4.0.15-alpha")
+    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
     testImplementation("org.testfx:testfx-junit:4.0.15-alpha")
     // Only needed for headless testing.
 //    testImplementation("org.testfx:openjfx-monocle:8u76-b04") // jdk-9+181 for Java 9
 
     // Spek
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.15")
-    testImplementation("org.spekframework.spek2:spek-runner-junit5:2.0.15")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.18")
+    testImplementation("org.spekframework.spek2:spek-runner-junit5:2.0.18")
 }
 
 repositories {
